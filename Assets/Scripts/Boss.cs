1f5b7535e283d92395b658326198f8328bd5a4a4
klcetines <laneulaestubular@gmail.com>
2025-08-03 18:23:34 +0200

    Boss a Mig Fer

---
+++ Assets/Scripts/Boss.cs
@@ -0,0 +1,49 @@
+using UnityEngine;
+
+public class Boss : MonoBehaviour
+{
+    [SerializeField] private GameObject gameManager;
+    [SerializeField] private float health = 10f;
+    [SerializeField] private GameObject ropePrefab;
+    [SerializeField] private Transform ropeSpawnPoint;
+    [SerializeField] private float attackInterval = 5f;
+
+    private float attackTimer = 0f;
+
+    void Update()
+    {
+        attackTimer += Time.deltaTime;
+        if (attackTimer >= attackInterval)
+        {
+            AttackWithRope();
+            attackTimer = 0f;
+        }
+    }
+
+    void AttackWithRope()
+    {
+        Instantiate(ropePrefab, ropeSpawnPoint.position, ropeSpawnPoint.rotation);
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            gameManager.GetComponent<HealthSystem>().TakeDamage("Player", 1f);
+        }
+        else if (other.CompareTag("Projectile"))
+        {
+            Destroy(other.gameObject);
+            health -= 1f;
+            if (health <= 0)
+            {
+                Destroy(gameObject);
+                Debug.Log("Boss defeated!");
+            }
+            else
+            {
+                Debug.Log($"Boss health remaining: {health}");
+            }
+        }
+    }
+}
